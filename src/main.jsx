1fdf695d9a63fd3edc34dfc8a676a087100e4b70
yunus kupucu <yunus.kupucu@gmail.com>
2025-07-10 14:38:24 +0300

    Add routing and initial components

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
 import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import routes from './routes';
 
-createRoot(document.getElementById('root')).render(<div>Test</div>);
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={routes} />
+);
