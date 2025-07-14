23560185922f96fa5a1a3e8b3ea1a612285e9171
yunus kupucu <yunus.kupucu@gmail.com>
2025-07-14 07:51:19 +0300

    created components for menu

---
+++ src/pages/not-found/index.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="bg-red-400 p-5 flex items-center justify-center">
+      Not Found Components
+    </div>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,42 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/home';
+import Explore from '../pages/explore';
+import Notifications from '../pages/notifications';
+import NotFound from '../pages/not-found';
+import Message from '../pages/message';
+import MainLayout from '../layouts/main';
+import Profile from '../pages/profile';
 
 const routes = createBrowserRouter([
   {
     path: '/',
-    element: 'anasayfa componenti',
-  },
-  {
-    path: '/explore',
-    element: 'explore componenti',
-  },
-  {
-    path: 'notifications/',
-    element: 'bildirimler componenti',
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'explore',
+        element: <Explore />,
+      },
+      {
+        path: 'notifications',
+        element: <Notifications />,
+      },
+      {
+        path: 'message',
+        element: <Message />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+    ],
   },
 ]);
 
